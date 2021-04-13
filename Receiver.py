705cf3595bb8fdf7cecd916e02a5d8d0c7350df2
Cristine <christinekhong8@gmail.com>
2021-04-13 11:55:24 +0700

    Update Receiver.py

--- Receiver.py
+++ Receiver.py
@@ -21,7 +21,7 @@ counter = 0
 # initial serial
 s = serial.Serial(
     port='/dev/ttyUSB0',
-    baudrate=9600
+    baudrate=9600,
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
     bytesize=serial.EIGHTBITS,
