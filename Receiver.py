004557ae60a103a95964d5815e69da4a92453b5d
Cristine <christinekhong8@gmail.com>
2021-04-13 13:45:05 +0700

    Update Receiver.py

--- Receiver.py
+++ Receiver.py
@@ -133,11 +133,11 @@ def InsertDb(x):
     detak = int(detak)
     oksigen = int(oksigen)
     suhu = float(suhu)
-
+    print("convert")
     queryInsert = (
         "INSERT INTO data (waktu, node, detak, oksigen, suhu) VALUES (%s, %s, %s, %s, %s)")
     values = (waktu, node, detak, oksigen, suhu)
-
+    print("query")
     cursor.execute(queryInsert, values)
     print("execute query")
     # commit data ke database
