9c072601ff442b4ebc7944999422fba6bdba45d7
Cristine <christinekhong8@gmail.com>
2021-04-26 23:39:20 +0700

    f

--- HealthCareApp/app/Views/pages/addPasien.php
+++ HealthCareApp/app/Views/pages/addPasien.php
@@ -6,14 +6,13 @@
         <div class="col-8">
             <h1 class="center mb-3">Form Tambah Pasien</h1>
             <!-- tampilin semua error $validation->listErrors();-->
-            <?= $validation->listError(); ?>
             <div class="container-md">
                 <form action="/Pasien/savePasien" method="POST" class="w-100">
                     <?= csrf_field(); ?>
                     <div class="form-group row mb-2">
                         <label for="nama" class="col-sm-2 col-form-label">Nama</label>
                         <div class="col-5">
-                            <input type="text" class="form-control <?= ($validation->hasError('judul')) ? 'is-invalid' : ''; ?>" id="nama" name="nama" autofocus value=<?= old('nama'); ?>>
+                            <input type="text" class="form-control <?= ($validation->hasError('nama')) ? 'is-invalid' : ''; ?>" id="nama" name="nama" autofocus value=<?= old('nama'); ?>>
                             <div class="invalid-feedback">
                                 <?= $validation->getError('nama'); ?>
                             </div>
