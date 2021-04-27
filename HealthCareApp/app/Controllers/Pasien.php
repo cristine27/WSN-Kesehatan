0eff4da91bec7c39bf3cd9361f101cc8088d2f7a
Cristine <christinekhong8@gmail.com>
2021-04-27 13:58:15 +0700

    Update Pasien.php

--- HealthCareApp/app/Controllers/Pasien.php
+++ HealthCareApp/app/Controllers/Pasien.php
@@ -121,6 +121,37 @@ class Pasien extends BaseController
 
     public function updatePasien($idPasien)
     {
+        //validasi input, ditarget berdasasrkan name form
+        if (!$this->validate([
+            'nama' => [
+                'rules' => 'required|alpha_space',
+                'errors' => [
+                    'required' => '{field} pasien harus diisi',
+                    'alpha_space' => '{field} pasien hanya diisi dapat diisi dengan huruf'
+                ]
+            ],
+            'usia' => [
+                'rules' => 'required|is_natural_no_zero|less_than_equal_to[100]|numeric',
+                'errors' => [
+                    'required' => '{field} pasien harus diisi',
+                    'is_natural_no_zero' => '{field} pasien tidak boleh 0',
+                    'numeric' => 'harus diisi dengan angka'
+                ]
+            ],
+            'gender' => [
+                'rules' => 'required',
+                'errors' => [
+                    'required' => '{field} harus diisi'
+                ]
+            ]
+        ])) {
+            //pesan error
+            $validation = \Config\Services::validation();
+            // dd($validation);
+            //mengirimkan validasi dan pesan error ke halaman add pasien
+            return redirect()->to('/Pasien/editPasien')->withInput()->with('validation', $validation);
+        }
+
         // dd($this->request->getVar());
         $this->pasienModel->save([
             'idPasien' => $idPasien,
