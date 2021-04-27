e8aeafb1e521981edf94f7ec8d7b0b0cf0f0df6c
Cristine <christinekhong8@gmail.com>
2021-04-27 14:09:22 +0700

    Update Routes.php
    edit dan delete pasien finish

--- HealthCareApp/app/Config/Routes.php
+++ HealthCareApp/app/Config/Routes.php
@@ -32,6 +32,7 @@ $routes->setAutoRoute(true);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Pages::index');
+// edit data pasien
 $routes->get('/Pasien/edit/(:segment)', 'Pasien::editPasien/$1');
 //routes untuk hapus pasien
 $routes->delete('/Pasien/(:num)', 'Pasien::deletePasien/$1');
