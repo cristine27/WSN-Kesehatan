d805d7fd7354a672aac3f983c9a19e96dc9e59fc
Cristine <christinekhong8@gmail.com>
2021-04-28 12:38:48 +0700

    Update pasienSeeder.php

--- HealthCareApp/app/Database/Seeds/pasienSeeder.php
+++ HealthCareApp/app/Database/Seeds/pasienSeeder.php
@@ -9,13 +9,78 @@ class pasienSeeder extends Seeder
     public function run()
     {
         $data = [
-            'nama'      => 'boo',
-            'alamat'    => 'jln abc no 1',
-            'umur'      => '34',
-            'gender'    => 'pria',
-            'email'     => 'boo@emai.com',
-            'password'  => 'momojjang'
-
+            [
+                'nama'      => 'mimi',
+                'alamat'    => 'jln abc no 1',
+                'umur'      => '34',
+                'gender'    => 'wanita',
+                'email'     => 'mimi@emai.com',
+                'password'  => 'momojjang'
+            ],
+            [
+                'nama'      => 'dahyun',
+                'alamat'    => 'jln abc no 1',
+                'umur'      => '34',
+                'gender'    => 'wanita',
+                'email'     => 'dahyun@emai.com',
+                'password'  => 'momojjang'
+            ],
+            [
+                'nama'      => 'budi',
+                'alamat'    => 'jln abc no 1',
+                'umur'      => '34',
+                'gender'    => 'pria',
+                'email'     => 'budi@emai.com',
+                'password'  => 'momojjang'
+            ],
+            [
+                'nama'      => 'doby',
+                'alamat'    => 'jln abc no 1',
+                'umur'      => '34',
+                'gender'    => 'pria',
+                'email'     => 'doby@emai.com',
+                'password'  => 'momojjang'
+            ],
+            [
+                'nama'      => 'kiki',
+                'alamat'    => 'jln abc no 1',
+                'umur'      => '34',
+                'gender'    => 'pria',
+                'email'     => 'kiki@emai.com',
+                'password'  => 'momojjang'
+            ],
+            [
+                'nama'      => 'jeni',
+                'alamat'    => 'jln abc no 1',
+                'umur'      => '34',
+                'gender'    => 'wanita',
+                'email'     => 'jeni@emai.com',
+                'password'  => 'momojjang'
+            ],
+            [
+                'nama'      => 'tine',
+                'alamat'    => 'jln abc no 1',
+                'umur'      => '34',
+                'gender'    => 'wanita',
+                'email'     => 'tine@emai.com',
+                'password'  => 'momojjang'
+            ],
+            [
+                'nama'      => 'vana',
+                'alamat'    => 'jln abc no 1',
+                'umur'      => '34',
+                'gender'    => 'wanita',
+                'email'     => 'vana@emai.com',
+                'password'  => 'momojjang'
+            ],
+            [
+                'nama'      => 'tina',
+                'alamat'    => 'jln abc no 1',
+                'umur'      => '34',
+                'gender'    => 'wanita',
+                'email'     => 'tina@emai.com',
+                'password'  => 'momojjang'
+            ]
         ];
 
         // Simple Queries
