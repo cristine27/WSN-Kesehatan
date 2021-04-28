31d5b2dd01d9a8c1a9816e611b143f81458d047a
Cristine <christinekhong8@gmail.com>
2021-04-28 15:11:03 +0700

    Update editPasien.php

--- HealthCareApp/app/Views/pages/editPasien.php
+++ HealthCareApp/app/Views/pages/editPasien.php
@@ -21,7 +21,7 @@
                     <div class="form-group row mb-2">
                         <label for="alamat" class="col-sm-2 col-form-label">Alamat</label>
                         <div class="col-5">
-                            <textarea class="form-control <?= ($validation->hasError('alamat')) ? 'is-invalid' : ''; ?>" aria-label="Alamat" name="alamat" value=<?= (old('alamat')) ? old('alamat') : $pasien['nama'] ?>></textarea>
+                            <textarea class="form-control <?= ($validation->hasError('alamat')) ? 'is-invalid' : ''; ?>" aria-label="Alamat" name="alamat" value=<?= (old('alamat')) ? old('alamat') : $pasien['alamat'] ?>></textarea>
                             <div class="invalid-feedback">
                                 <?= $validation->getError('alamat'); ?>
                             </div>
