2c2b89c84a79c0bb678e2adec5d9731dd5b7db3b
Cristine <christinekhong8@gmail.com>
2021-05-01 15:31:34 +0700

    Update SignIn.php

--- HealthCareApp/app/Controllers/SignIn.php
+++ HealthCareApp/app/Controllers/SignIn.php
@@ -62,7 +62,7 @@ class SignIn extends BaseController
                 // dd($idParam);
                 foreach ($idParam as $id) {
                     d("masuk");
-                    d($this->parameterModel->getNamaParam($id['idNode']));
+                    d($this->parameterModel->getNamaParam($id['idParam']));
                 }
 
                 $data = [
