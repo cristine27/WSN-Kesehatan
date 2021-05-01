c9421f95b452f1414166b3466d993a31903bb7d8
Cristine <christinekhong8@gmail.com>
2021-05-01 15:57:30 +0700

    Update homePasien.php

--- HealthCareApp/app/Views/pages/homePasien.php
+++ HealthCareApp/app/Views/pages/homePasien.php
@@ -8,7 +8,7 @@
             <div class="container">
                 <div class="row">
                     <h5 class="mt-5">Hasil Pemeriksaan : <?= $hasilPeriksa['waktu']; ?></h5>
-                    <?php d($parameter); ?>
+                    <?php dd($parameter); ?>
                     <div class="col d-flex justify-content-start">
                         <div class="w-75 table-responsive">
                             <table class="table table-striped text-center">
