feda182b8a1269bb85e73f53af48e22e85a6aad8
Cristine <christinekhong8@gmail.com>
2021-05-04 15:40:43 +0700

    fix layout

--- HealthCareApp/app/Views/pages/homePasien.php
+++ HealthCareApp/app/Views/pages/homePasien.php
@@ -9,16 +9,16 @@
             <h1 class="display-4">Health Care</h1>
             <div class="col">
                 <div class="container">
-                    <div class="jumbotron jumbotron-fluid" style="background-color: beige;">
+                    <div class="jumbotron jumbotron-fluid h-75" style="background-color: beige;">
                         <p class="lead">Selamat datang <?= ($dataPasien['gender'] == "Pria") ? "Bapak " : "Ibu "; ?> <?= $dataPasien['nama']; ?></p>
                         <hr class="my-4">
                         <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
                         <div class="container">
                             <!-- <div class="col"> -->
                             <div class="d-flex justify-content-center">
-                                <a class="btn btn-primary btn-lg d-inline p-2" href="../Home/getHasilPantau" role="button">Hasil Pemantuan</a>
-                                <a class="btn btn-primary btn-lg d-inline p-2" href="../Home/getPasienProfile" role="button">Profile</a>
-                                <a class="btn btn-primary btn-lg d-inline p-2" href="/SignIn" role="button">Log out</a>
+                                <a class="btn btn-primary btn-lg d-inline p-2 mx-3" href="../Home/getHasilPantau" role="button">Hasil Pemantuan</a>
+                                <a class="btn btn-primary btn-lg d-inline p-2 mx-3" href="../Home/getPasienProfile" role="button">Profile</a>
+                                <a class="btn btn-primary btn-lg d-inline p-2 mx-3" href="/SignIn" role="button">Log out</a>
                             </div>
                         </div>
                     </div>
