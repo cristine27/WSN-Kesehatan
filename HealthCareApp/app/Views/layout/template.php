c51987781ed6e50c27e69926397b34e70de9a065
Cristine <christinekhong8@gmail.com>
2021-05-04 15:54:54 +0700

    Update template.php

--- HealthCareApp/app/Views/layout/template.php
+++ HealthCareApp/app/Views/layout/template.php
@@ -7,7 +7,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- my css -->
-    <link rel="stylesheet" href="/public/css/style.css">
+    <style>
+        body {
+            height: 100%;
+            width: 100%;
+            background-color: violet;
+        }
+    </style>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
