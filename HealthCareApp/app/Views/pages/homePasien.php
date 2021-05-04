1934bd8477ccca8985790d71c0ef5403ef330eb7
Cristine <christinekhong8@gmail.com>
2021-05-04 12:23:52 +0700

    Update homePasien.php

--- HealthCareApp/app/Views/pages/homePasien.php
+++ HealthCareApp/app/Views/pages/homePasien.php
@@ -20,18 +20,24 @@
 <body>
     <div class="container">
         <div class="row">
-            <div class="col">
-                <div class="jumbotron jumbotron-fluid mx-auto">
+            <div class="col mx-auto">
+                <div class="jumbotron jumbotron-fluid">
 
                     <h1 class="display-4">Health Care</h1>
                     <p class="lead">Selamat datang <?= ($dataPasien['gender'] == "Pria") ? "Bapak " : "Ibu "; ?> <?= $dataPasien['nama']; ?></p>
                     <hr class="my-4">
                     <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                    <div class="container">
+                        <div class="row d-inline">
+                            <a class="btn btn-primary btn-lg" href="getHasilPantau/<?= $dataPasien['idPasien']; ?>" role="button">Hasil Pemantuan</a>
+                            <a class="btn btn-primary btn-lg" href="getPasienProfile/<?= $dataPasien['idPasien']; ?>" role="button">Profile</a>
+                        </div>
+                    </div>
                     <p class="lead">
-                        <a class="btn btn-primary btn-lg" href="getHasilPantau/<?= $dataPasien['idPasien']; ?>" role="button">Hasil Pemantuan</a>
+
                     </p>
                     <p class="lead">
-                        <a class="btn btn-primary btn-lg" href="getPasienProfile/<?= $dataPasien['idPasien']; ?>" role="button">Profile</a>
+
                     </p>
                 </div>
             </div>
