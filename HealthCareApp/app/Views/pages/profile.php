9687a5cbc725b440562e316522e3fe81f2513c1d
Cristine <christinekhong8@gmail.com>
2021-05-04 14:38:31 +0700

    Update profile.php

--- HealthCareApp/app/Views/pages/profile.php
+++ HealthCareApp/app/Views/pages/profile.php
@@ -2,11 +2,11 @@
 
 <?= $this->section('content'); ?>
 
-<div class="container-fluid">
+<div class="container">
     <div class="row">
         <div class="col">
             <div class="jumbotron jumbotron-fluid">
-                <div class="container">
+                <div class="container-fluid">
                     <?php d($dataPasien); ?>
                     <h1 class="display-4">Profile Pasien</h1>
                     <!-- <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p> -->
@@ -18,11 +18,60 @@
                     <P>Jenis Kelamin : <?= $dataPasien['gender']; ?></P>
                     <br>
                     <P>Umur : <?= $dataPasien['umur']; ?></P>
+                    <div class="col-md-8">
+                        <div class="card mb-3">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-sm-3">
+                                        <h6 class="mb-0">Nama</h6>
+                                    </div>
+                                    <div class="col-sm-9 text-secondary">
+                                        <?= $dataPasien['nama']; ?>
+                                    </div>
+                                </div>
+                                <hr>
+                                <div class="row">
+                                    <div class="col-sm-3">
+                                        <h6 class="mb-0">Email</h6>
+                                    </div>
+                                    <div class="col-sm-9 text-secondary">
+                                        <?= $dataPasien['email']; ?>
+                                    </div>
+                                </div>
+                                <hr>
+                                <div class="row">
+                                    <div class="col-sm-3">
+                                        <h6 class="mb-0">Gender</h6>
+                                    </div>
+                                    <div class="col-sm-9 text-secondary">
+                                        <?= $dataPasien['gender']; ?>
+                                    </div>
+                                </div>
+                                <hr>
+                                <div class="row">
+                                    <div class="col-sm-3">
+                                        <h6 class="mb-0">Umur</h6>
+                                    </div>
+                                    <div class="col-sm-9 text-secondary">
+                                        <?= $dataPasien['umur']; ?>
+                                    </div>
+                                </div>
+                                <hr>
+                                <div class="row">
+                                    <div class="col-sm-3">
+                                        <h6 class="mb-0">Alamat</h6>
+                                    </div>
+                                    <div class="col-sm-9 text-secondary">
+                                        <?= $dataPasien['alamat']; ?>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
+            <a class="float-lg-right" href="/Home">Kembali</a>
         </div>
-        <a class="float-lg-right" href="/Home">Kembali</a>
     </div>
-</div>
 
-<?= $this->endsection(); ?>
+    <?= $this->endsection(); ?>
