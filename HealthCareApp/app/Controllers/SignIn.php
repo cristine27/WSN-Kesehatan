e89b329a7141d24c07677ebf0915cc86a083c0cc
Cristine <christinekhong8@gmail.com>
2021-05-04 12:01:14 +0700

    Update SignIn.php

--- HealthCareApp/app/Controllers/SignIn.php
+++ HealthCareApp/app/Controllers/SignIn.php
@@ -115,6 +115,6 @@ class SignIn extends BaseController
             // 'idParam' => $idParam
         ];
 
-        return view('pages/homePasien', $data);
+        return view('pages/pemantauanPasien', $data);
     }
 }
