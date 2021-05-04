ef1f61d117dc18877c59c1b8b05b2937f3d46704
Cristine <christinekhong8@gmail.com>
2021-05-04 15:30:29 +0700

    typos link

--- HealthCareApp/app/Views/pages/homePasien.php
+++ HealthCareApp/app/Views/pages/homePasien.php
@@ -6,27 +6,22 @@
 <body>
     <div class="container">
         <div class="row">
+            <h1 class="display-4">Health Care</h1>
             <div class="col">
                 <div class="jumbotron jumbotron-fluid d-flex justify-content-center" style="background-color: beige;">
-
-                    <h1 class="display-4">Health Care</h1>
                     <p class="lead">Selamat datang <?= ($dataPasien['gender'] == "Pria") ? "Bapak " : "Ibu "; ?> <?= $dataPasien['nama']; ?></p>
                     <hr class="my-4">
                     <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-                    <div class="container">
-                        <!-- <div class="col"> -->
-                        <div class="col d-flex justify-content-center">
-                            <a class="btn btn-primary btn-lg d-inline p-2" href="../Home/getHasilPantau" role="button">Hasil Pemantuan</a>
-                            <a class="btn btn-primary btn-lg d-inline p-2" href="../Home/getPasienProfile" role="button">Profile</a>
-                            <a class="btn btn-primary btn-lg d-inline p-2" href="/SignIn" role="button">Log out</a>
-                        </div>
-                    </div>
-                    <p class="lead">
-
-                    </p>
-                    <p class="lead">
-
-                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="container">
+                <!-- <div class="col"> -->
+                <div class="col d-flex justify-content-center">
+                    <a class="btn btn-primary btn-lg d-inline p-2" href="../Home/getHasilPantau" role="button">Hasil Pemantuan</a>
+                    <a class="btn btn-primary btn-lg d-inline p-2" href="../Home/getPasienProfile" role="button">Profile</a>
+                    <a class="btn btn-primary btn-lg d-inline p-2" href="/SignIn" role="button">Log out</a>
                 </div>
             </div>
         </div>
