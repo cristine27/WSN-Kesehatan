912ffa7dce17c1781d6bc4743cc612e1490f853b
Cristine <christinekhong8@gmail.com>
2021-05-06 15:13:47 +0700

    Update detailPasien.php

--- HealthCareApp/app/Views/pages/detailPasien.php
+++ HealthCareApp/app/Views/pages/detailPasien.php
@@ -42,7 +42,7 @@
                                     </thead>
                                     <?php
                                     $index = 0;
-                                    // d($parameter);
+                                    dd($status);
                                     for ($i = 0; $i < count($parameter); $i++) { ?>
                                         <tbody>
                                             <tr>
