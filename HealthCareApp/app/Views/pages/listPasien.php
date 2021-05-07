be4bc3cc26f541220ff4fa2cfc41ae555cc5cdcc
Cristine <christinekhong8@gmail.com>
2021-05-07 21:02:00 +0700

    Update listPasien.php

--- HealthCareApp/app/Views/pages/listPasien.php
+++ HealthCareApp/app/Views/pages/listPasien.php
@@ -22,43 +22,43 @@
         <div class="row">
             <div class="col">
                 <a href="/Pasien/addPasien" class="btn btn-primary mt-3 d-inline">Tambah Pasien</a>
-                <a role="button" class="btn btn-danger float-end d-inline" href="/SignIn">Log Out</button>
-                    <?php if (session()->getFlashdata('pesan')) : ?>
-                        <div class="alert alert-success" role="alert">
-                            <?= session()->getFlashdata('pesan'); ?>
-                        </div>
-                    <?php endif; ?>
+                <a href="/SignIn" role="button" class="btn btn-danger float-end d-inline">Log Out</a>
+                <?php if (session()->getFlashdata('pesan')) : ?>
+                    <div class="alert alert-success" role="alert">
+                        <?= session()->getFlashdata('pesan'); ?>
+                    </div>
+                <?php endif; ?>
 
 
-                    <table class="table">
-                        <?php $i = 1 + (6 * ($currentPage - 1)) ?>
-                        <thead>
+                <table class="table">
+                    <?php $i = 1 + (6 * ($currentPage - 1)) ?>
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">idPasien</th>
+                            <th scope="col">Nama</th>
+                            <th scope="col">Alamat</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Password</th>
+
+                            <th scope="col">Detail</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($pasien as $p) : ?>
                             <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">idPasien</th>
-                                <th scope="col">Nama</th>
-                                <th scope="col">Alamat</th>
-                                <th scope="col">Email</th>
-                                <th scope="col">Password</th>
-
-                                <th scope="col">Detail</th>
+                                <th scope="row"><?= $i++; ?></th>
+                                <td><?= $p['idPasien']; ?></td>
+                                <td><?= $p['nama']; ?></td>
+                                <td><?= $p['alamat']; ?></td>
+                                <td><?= $p['email']; ?></td>
+                                <td><?= $p['password']; ?></td>
+                                <td><a href="/Pasien/detail/<?= $p['idPasien']; ?>" class="btn btn-success">Detail</a></td>
                             </tr>
-                        </thead>
-                        <tbody>
-                            <?php foreach ($pasien as $p) : ?>
-                                <tr>
-                                    <th scope="row"><?= $i++; ?></th>
-                                    <td><?= $p['idPasien']; ?></td>
-                                    <td><?= $p['nama']; ?></td>
-                                    <td><?= $p['alamat']; ?></td>
-                                    <td><?= $p['email']; ?></td>
-                                    <td><?= $p['password']; ?></td>
-                                    <td><a href="/Pasien/detail/<?= $p['idPasien']; ?>" class="btn btn-success">Detail</a></td>
-                                </tr>
-                            <?php endforeach; ?>
-                        </tbody>
-                    </table>
-                    <?= $pager->links('pasien', 'pasien_pagination'); ?>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+                <?= $pager->links('pasien', 'pasien_pagination'); ?>
             </div>
         </div>
     </div>
