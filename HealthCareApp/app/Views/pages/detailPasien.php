1f06338756d03364c16232877729ce4cd9584631
Cristine <christinekhong8@gmail.com>
2021-05-19 21:21:47 +0700

    Update detailPasien.php

--- HealthCareApp/app/Views/pages/detailPasien.php
+++ HealthCareApp/app/Views/pages/detailPasien.php
@@ -26,6 +26,19 @@
                         </form>
                     </div>
                 </div>
+                <p>
+                    <a class="btn btn-primary" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+                        Link with href
+                    </a>
+                    <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                        Button with data-target
+                    </button>
+                </p>
+                <div class="collapse" id="collapseExample">
+                    <div class="card card-body">
+                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+                    </div>
+                </div>
                 <!-- <div class="card w-75 mt-5"> -->
                 <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
                     Riwayat Pemeriksaan
