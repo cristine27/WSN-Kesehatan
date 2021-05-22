94dba7e91888e5373fc9b9d81fbfecb92b7fccd0
Cristine <christinekhong8@gmail.com>
2021-05-22 20:58:40 +0700

    Update riwayatPasien.php

--- HealthCareApp/app/Views/pages/riwayatPasien.php
+++ HealthCareApp/app/Views/pages/riwayatPasien.php
@@ -11,8 +11,8 @@
             </div>
         </div>
         <?php
-        foreach ($hasilPeriksa as $key => $value) {
-            // d($value['waktu']);
+        foreach ($parameter as $key => $value) {
+            d($value['namaParameter']);
         }
         ?>
         <!-- <div class="row">
@@ -50,7 +50,8 @@
                     <h4 class="card-header">Riwayat Pemeriksaan</h4>
                     <div class="card-body">
                         <p class="<?= ($flag == true) ? 'invisible' : 'visible'; ?>">Pasien Belum Melakukan Pemeriksaan</p>
-                        <?php for ($index = 0; $index < $jumlahHasil; $index++) { ?>
+                        <?php foreach ($hasilPeriksa as $key => $value) {
+                            $index = 0; ?>
                             <h5 class="mt-5">Hasil Pemeriksaan : <?= $hasilPeriksa[$index]['waktu']; ?></h5>
                             <div class="col d-flex justify-content-start">
                                 <div class="w-75 table-responsive">
@@ -67,13 +68,14 @@
                                             <tbody>
                                                 <tr>
                                                     <th scope="row"><?= $parameter[$index][$i]['namaParameter']; ?></th>
-                                                    <td><?= $hasilPeriksa[$index]['hasil' . strval($i + 1)]; ?></td>
+                                                    <td><?= $value['hasil' . strval($i + 1)]; ?></td>
                                                     <td>
                                                         <p class="text-<?= $status[$index][$i] == "normal" ? 'success' : 'danger'; ?>"><?= $status[$index][$i]; ?></p>
                                                     </td>
                                                 </tr>
                                             </tbody>
-                                        <?php } ?>
+                                        <?php }
+                                        $index++; ?>
                                     </table>
                                 </div>
                             </div>
