401acf7bbd7497578054b74bdc5b7b8b6a2c8bec
Cristine <christinekhong8@gmail.com>
2021-05-22 21:04:28 +0700

    Update riwayatPasien.php

--- HealthCareApp/app/Views/pages/riwayatPasien.php
+++ HealthCareApp/app/Views/pages/riwayatPasien.php
@@ -48,42 +48,43 @@
         </div> -->
         <div class="row">
             <div class="col">
-                <div class="card w-75 mt-5">
-                    <h4 class="card-header">Riwayat Pemeriksaan</h4>
-                    <div class="card-body">
-                        <p class="<?= ($flag == true) ? 'invisible' : 'visible'; ?>">Pasien Belum Melakukan Pemeriksaan</p>
-                        <?php foreach ($hasilPeriksa as $key => $value) {
-                            $index = 0; ?>
-                            <h5 class="mt-5">Hasil Pemeriksaan : <?= $hasilPeriksa[$index]['waktu']; ?></h5>
-                            <div class="col d-flex justify-content-start">
-                                <div class="w-75 table-responsive">
-                                    <table id="table_riwayat" class="table table-striped text-center">
-                                        <thead class="table-dark">
-                                            <tr>
-                                                <th scope="col">Parameter</th>
-                                                <th scope="col">Hasil</th>
-                                                <th scope="col">Status</th>
-                                            </tr>
-                                        </thead>
-                                        <?php
-                                        for ($i = 0; $i < 3; $i++) { ?>
-                                            <tbody>
-                                                <tr>
-                                                    <th scope="row"><?= $parameter[$index][$i]['namaParameter']; ?></th>
-                                                    <td><?= $value['hasil' . strval($i + 1)]; ?></td>
-                                                    <td>
-                                                        <p class="text-<?= $status[$index][$i] == "normal" ? 'success' : 'danger'; ?>"><?= $status[$index][$i]; ?></p>
-                                                    </td>
-                                                </tr>
-                                            </tbody>
-                                        <?php }
-                                        $index++; ?>
-                                    </table>
-                                </div>
-                            </div>
-                        <?php } ?>
+                <h1>Riwayat Pemeriksaan</h1>
+                <!-- <div class="card w-75 mt-5"> -->
+                <!-- <h4 class="card-header">Riwayat Pemeriksaan</h4> -->
+                <!-- <div class="card-body"> -->
+                <p class="<?= ($flag == true) ? 'invisible' : 'visible'; ?>">Pasien Belum Melakukan Pemeriksaan</p>
+                <?php foreach ($hasilPeriksa as $key => $value) {
+                    $index = 0; ?>
+                    <h5 class="mt-5">Hasil Pemeriksaan : <?= $hasilPeriksa[$index]['waktu']; ?></h5>
+                    <div class="col d-flex justify-content-start">
+                        <div class="w-75 table-responsive">
+                            <table id="table_riwayat" class="table table-striped text-center">
+                                <thead class="table-dark">
+                                    <tr>
+                                        <th scope="col">Parameter</th>
+                                        <th scope="col">Hasil</th>
+                                        <th scope="col">Status</th>
+                                    </tr>
+                                </thead>
+                                <?php
+                                for ($i = 0; $i < 3; $i++) { ?>
+                                    <tbody>
+                                        <tr>
+                                            <th scope="row"><?= $parameter[$index][$i]['namaParameter']; ?></th>
+                                            <td><?= $value['hasil' . strval($i + 1)]; ?></td>
+                                            <td>
+                                                <p class="text-<?= $status[$index][$i] == "normal" ? 'success' : 'danger'; ?>"><?= $status[$index][$i]; ?></p>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                <?php }
+                                $index++; ?>
+                            </table>
+                        </div>
                     </div>
-                </div>
+                <?php } ?>
+                <!-- </div> -->
+                <!-- </div> -->
             </div>
         </div>
     </div>
