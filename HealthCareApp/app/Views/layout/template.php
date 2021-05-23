0c3482ba90e64a074a7370cc7c9e070de605e8de
Cristine <christinekhong8@gmail.com>
2021-05-23 22:20:48 +0700

    hapus file yang tidak digunakan

--- HealthCareApp/app/Views/layout/template.php
+++ HealthCareApp/app/Views/layout/template.php
@@ -14,32 +14,16 @@
         }
     </style>
 
-    <!-- Bootstrap -->
-    <!-- <link rel='stylesheet' type='text/css' href='../Bootstrap 4/bootstrap.min.css' />
-    <script src="../Bootstrap 4/jquery.min.js"></script>
-    <script src="../Bootstrap 4/popper.min.js"></script>
-    <script src="../Bootstrap 4/bootstrap.min.js"></script> -->
 
-    <!-- Data Table -->
-    <script rel='stylesheet' src="../dataTables/jquery.dataTables.min.js"></script>
-    <script rel='stylesheet' src="../dataTables/dataTables.bootstrap4.min.js"></script>
-    <script rel='stylesheet' src="../dataTables/dataTables.responsive.min.js"></script>
-    <script rel='stylesheet' src="https://cdn.datatables.net/1.10.24/js/dataTables.bootstrap5.min.js"></script>
-    <!-- <script src="../dataTables/responsive.bootstrap4.min.js"></script> -->
-    <link rel='stylesheet' type='text/css' href='../dataTables/dataTables.bootstrap4.min.css' />
-    <link rel='stylesheet' type='text/css' href='../dataTables/responsive.bootstrap4.min.css' />
-
-    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS -->
+    <!-- jQuery, Popper.js, and Bootstrap JS -->
     <script src="https://cdn.datatables.net/1.10.24/js/dataTables.bootstrap5.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
-    <!--jquery  -->
+
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
 
-    <!-- my js -->
-    <script src="../js/script.js"></script>
 
     <title><?= $title; ?></title>
 
