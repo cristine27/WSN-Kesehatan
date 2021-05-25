442dda68a2110b5cd203baa843125f84cbd921ec
Cristine <christinekhong8@gmail.com>
2021-05-25 23:06:25 +0700

    Update signin.php

--- HealthCareApp/app/Views/pages/signin.php
+++ HealthCareApp/app/Views/pages/signin.php
@@ -23,17 +23,17 @@
 
                                     <!-- <h1 class="h3 mb-3 fw-normal">Please sign in</h1> -->
                                     <div class="md-form mt-1">
-                                        <i class="bi bi-envelope"></i>
-                                        <label for="email" class="fa fa-envelope prefix grey-text">Email</label>
-                                        <input type="email" id="email" name="email" class="form-control <?= ($validate == 'false') ? 'is-invalid' : ''; ?>" placeholder="Email address" required autofocus>
+                                        <i class="fa fa-envelope" aria-hidden="true"></i>
                                     </div>
+                                    <i class="bi bi-envelope"></i>
+                                    <label for="email" class="fa fa-envelope prefix grey-text">Email</label>
+                                    <input type="email" id="email" name="email" class="form-control <?= ($validate == 'false') ? 'is-invalid' : ''; ?>" placeholder="Email address" required autofocus>
 
                                     <div class="md-form mt-1">
-                                        <i class="bi bi-lock"></i>
-                                        <label for="password" class="fa fa-lock prefix grey-text">Password</label>
-                                        <input type="password" id="password" name="password" class="form-control <?= ($validate == 'false') ? 'is-invalid' : ''; ?>" placeholder="Password" required>
                                     </div>
-
+                                    <i class="bi bi-lock"></i>
+                                    <label for="password" class="fa fa-lock prefix grey-text">Password</label>
+                                    <input type="password" id="password" name="password" class="form-control <?= ($validate == 'false') ? 'is-invalid' : ''; ?>" placeholder="Password" required>
                                     <div class="invalid-feedback my-1">
                                         <b><?= $pesan; ?></b>
                                     </div>
