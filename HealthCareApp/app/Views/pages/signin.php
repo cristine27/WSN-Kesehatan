c5079ede43c6beb79b29d4b67b46c14f26f7b558
Cristine <christinekhong8@gmail.com>
2021-05-25 23:13:09 +0700

    Update signin.php

--- HealthCareApp/app/Views/pages/signin.php
+++ HealthCareApp/app/Views/pages/signin.php
@@ -20,23 +20,25 @@
 
                                 <form method="POST" action="/SignIn/validateLogin">
                                     <?= csrf_field(); ?>
-
+                                    <div class="form-group row">
+                                        <label for="email" class="fa fa-envelope prefix grey-text">Email</label>
+                                        <div class="col-sm-10">
+                                            <input type="email" id="email" name="email" class="form-control <?= ($validate == 'false') ? 'is-invalid' : ''; ?>" placeholder="Email address" required autofocus>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="password" class="fa fa-lock prefix grey-text">Password</label>
+                                        <div class="col-sm-10">
+                                            <input type="password" id="password" name="password" class="form-control <?= ($validate == 'false') ? 'is-invalid' : ''; ?>" placeholder="Password" required>
+                                        </div>
+                                    </div>
                                     <!-- <h1 class="h3 mb-3 fw-normal">Please sign in</h1> -->
-                                    <div class="md-form mt-1">
-                                        <i class="fa fa-envelope" aria-hidden="true"></i>
-                                    </div>
-                                    <i class="bi bi-envelope"></i>
-                                    <label for="email" class="fa fa-envelope prefix grey-text">Email</label>
-                                    <input type="email" id="email" name="email" class="form-control <?= ($validate == 'false') ? 'is-invalid' : ''; ?>" placeholder="Email address" required autofocus>
 
-                                    <div class="md-form mt-1">
-                                    </div>
-                                    <i class="bi bi-lock"></i>
-                                    <label for="password" class="fa fa-lock prefix grey-text">Password</label>
-                                    <input type="password" id="password" name="password" class="form-control <?= ($validate == 'false') ? 'is-invalid' : ''; ?>" placeholder="Password" required>
+
                                     <div class="invalid-feedback my-1">
                                         <b><?= $pesan; ?></b>
                                     </div>
+
                                     <button class="btn btn-info my-2" value="Submit">Sign In</button>
                                 </form>
                             </div>
