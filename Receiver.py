7abb37e1267f7c9884f713b7805b858da7ed16a0
Cristine <christinekhong8@gmail.com>
2021-05-26 21:56:20 +0700

    Update Receiver.py

--- Receiver.py
+++ Receiver.py
@@ -567,7 +567,7 @@ while appRunning:
                 # ambil data sensing arduino
                 msg = s.readline().decode("ascii").strip()
                 counter = counter + 1
-                time.sleep(5)
+                time.sleep(1)
                 with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
                     #check apakah alat terpasang dengan benar
                     time.sleep(5)
