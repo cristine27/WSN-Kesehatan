7c7cb1e4bc18915aef9f95ade322b95ce9fcdebb
Cristine <christinekhong8@gmail.com>
2021-05-30 22:48:00 +0700

    Update Receiver.py

--- Receiver.py
+++ Receiver.py
@@ -527,7 +527,7 @@ POOL_SIZE = 20
 
 while appRunning:
     mapNodeName()
-    
+    statusN()
     while menuShow: 
         # statusN()
         print(" ")
@@ -548,7 +548,7 @@ while appRunning:
                 formatPasien = input()
 
                 print("Mohon tunggu Assign pasien sedang dilakukan..")
-                statusN()
+                # statusN()
                 
                 insertDataNodePasien(formatPasien,jumlahPasien)
                 print(StatusInput)
