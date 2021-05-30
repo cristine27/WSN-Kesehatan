d39c16d5f02d6e9bc008466bca771ba665f1cf7b
Cristine <christinekhong8@gmail.com>
2021-05-30 20:48:31 +0700

    coba tanpa time sleep

--- Receiver.py
+++ Receiver.py
@@ -529,14 +529,14 @@ while appRunning:
                     with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
                         msg = s.readline().decode("ascii").strip()
                         #lakukan pengecekan apakah sensor terpasang dengan benar pada tubuh pasien
-                        time.sleep(5)
+                        # time.sleep(5)
                         status = checkIfAttached(msg)
                         if status==False:
-                            time.sleep(5)
+                            # time.sleep(5)
                             future = executor.submit(getDataSense, msg)
-                            time.sleep(5)
+                            # time.sleep(5)
                             data = future.result()
-                            time.sleep(5)
+                            # time.sleep(5)
                             if future.done() and data != None:
                                 future2 = executor.submit(InsertDb, data)
                             
